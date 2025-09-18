eaf229d9227aaebd5746cb314bcaf14156e433ce
DemF <viktop.a@yandex.ru>
2025-09-18 17:44:34 +0300

    add dict class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,19 @@
 package org.example;
 
-public class App
+import java.util.Dictionary;
+import java.util.Map;
+
+public class Main
 {
     public static void main( String[] args ) {
-        Hangman game = new Hangman();
+        /*Hangman game = new Hangman();
         System.out.println(game.play.remove());
         System.out.println(game.play.remove());
         System.out.println(game.play.remove());
         System.out.println(game.play.remove());
         System.out.println(game.play.remove());
-        System.out.println(game.play.remove());
+        System.out.println(game.play.remove());*/
+        new Dict().pr();
 
     }
 }
