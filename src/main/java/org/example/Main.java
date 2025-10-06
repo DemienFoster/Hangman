fbfb421d0bced83d80673922777983d2ba82aa3d
DemF <viktop.a@yandex.ru>
2025-10-06 22:24:32 +0300

    test some features with inbound files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,17 @@
 package org.example;
 
+import java.io.File;
+import java.util.Dictionary;
+
 public class Main
 {
     public static void main( String[] args ) {
-        Game game = new Game();
-        game.start();
+        /*Game game = new Game();
+        game.start();*/
+        System.out.println(System.getProperty("user.dir"));
+
+        FileDictionary fd = new FileDictionary();
+        //fd.CheckErrorStringsFromFile("Dictionary.txt");
+        fd.countStringsInFile();
     }
 }
