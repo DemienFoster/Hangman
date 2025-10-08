5a84333900986866c6e8263fddba094129cef6ad
DemF <viktop.a@yandex.ru>
2025-10-08 17:34:02 +0300

    change random word choice

--- src/main/java/org/example/FileDictionary.java
+++ src/main/java/org/example/FileDictionary.java
@@ -1,28 +1,30 @@
 package org.example;
 
 import java.io.*;
+import java.util.Random;
 import java.util.regex.Pattern;
 
 public class FileDictionary {
 
-    int lineNumber = 0;
+    private final int NUMBER_OF_LINES = 48777;
+    private String selectWord;
 
-    public void countStringsInFile () {
-
+    public String getSelectWord () {
         try (BufferedReader reader = new BufferedReader(new FileReader("out.txt"))) {
-
-
-            while (reader.readLine() != null) {
+            String selectWord;
+            int lineNumber = 0;
+            int randomLineNumber = new Random().nextInt(NUMBER_OF_LINES);
+            while ((selectWord = reader.readLine()) != null) {
                 lineNumber++;
+                if(lineNumber == randomLineNumber) {
+                    return selectWord;
+                }
             }
-            System.out.println("---------------------\n\n");
-            System.out.println("Всего строк: " + lineNumber);
-            System.out.println("\n\n-----------------------");
-
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        return null;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,6 @@ public class Main
 
         FileDictionary fd = new FileDictionary();
         //fd.CheckErrorStringsFromFile("Dictionary.txt");
-        fd.countStringsInFile();
+        System.out.println(fd.getSelectWord());
     }
 }
